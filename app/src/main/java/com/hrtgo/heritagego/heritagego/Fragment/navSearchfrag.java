be9696ea11d387f28a7ecb41a42f5efa87ad07c0
TranTruyen <truyen.tranbao@gmail.com>
2018-06-06 18:02:20 +0700

    Android 1.0.4.3
    Search function

---
+++ app/src/main/java/com/hrtgo/heritagego/heritagego/Fragment/navSearchfrag.java
@@ -0,0 +1,169 @@
+package com.hrtgo.heritagego.heritagego.Fragment;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.hrtgo.heritagego.heritagego.Activity.LocationDetail;
+import com.hrtgo.heritagego.heritagego.Adapter.rcvAdapterTabsHome;
+import com.hrtgo.heritagego.heritagego.Interface.Json;
+import com.hrtgo.heritagego.heritagego.Model.heritageInfoHomeModel;
+import com.hrtgo.heritagego.heritagego.R;
+import com.hrtgo.heritagego.heritagego.Worker.VolleySingleton;
+import com.hrtgo.heritagego.heritagego.untill.customize;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+import okhttp3.ResponseBody;
+
+public class navSearchfrag extends Fragment {
+
+    EditText edtSearch;
+    RecyclerView recyclerView;
+    ArrayList<heritageInfoHomeModel> listData;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        listData = new ArrayList<>();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.nav_bottom_search_fragment, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view){
+        recyclerView = view.findViewById(R.id.rcv_search);
+        editextSearchEvent(view);
+    }
+
+    private void editextSearchEvent(View view){
+        edtSearch = view.findViewById(R.id.txt_search);
+        edtSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                if(actionId == EditorInfo.IME_ACTION_SEARCH){
+                    listData.clear();
+                    setSearchRecyclerView(listData);
+                    try {
+                        callAPI("1", edtSearch.getText().toString());
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+//    private void setFocusEditSearch(){
+//        edtSearch.setFocusableInTouchMode(true);
+//        edtSearch.requestFocus();
+//        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+//        ((InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(edtSearch, 0);
+//    }
+
+    private void setSearchRecyclerView(ArrayList<heritageInfoHomeModel> listData){
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        final rcvAdapterTabsHome adapter = new rcvAdapterTabsHome(listData, this.getContext());
+        recyclerView.setAdapter(adapter);
+
+        adapter.notifyDataSetChanged();
+    }
+
+    private void getListData(String url, final Json json){
+
+        StringRequest jsonRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e("search", response);
+                json.parseJson(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        VolleySingleton.getInStance(this.getContext()).getRequestQueue().add(jsonRequest);
+    }
+
+    private void callAPI(String currentPage, String txtSearch) throws UnsupportedEncodingException {
+        String search = URLEncoder.encode(txtSearch, "UTF-8");
+        String url = getActivity().getResources().getString(R.string.request_search) + search + "/" + currentPage ;
+
+        getListData(url, new Json() {
+            @Override
+            public void parseJson(String jsonString) {
+                try {
+                    JSONObject root = new JSONObject(jsonString);
+
+                    JSONArray pdataArray = root.getJSONArray("pdata");
+
+                    for (int i = 0; i < pdataArray.length(); i++){
+                        JSONObject location = pdataArray.getJSONObject(i);
+
+                        listData.add(new heritageInfoHomeModel(location.getInt("ID")
+                                ,location.getString("Name")
+                                ,location.getInt("Liked")
+                                ,location.getInt("Viewed")
+                                ,location.getString("ImagePath")));
+                    }
+
+                } catch (JSONException e) {
+                }
+                setSearchRecyclerView(listData);
+            }
+        });
+    }
+}
